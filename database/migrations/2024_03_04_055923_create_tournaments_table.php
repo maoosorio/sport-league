64d842bc561c2b1e3f2be014c3b5065cdd50fa55
Mario Alberto Solórzano Osorio <isc_osorio@hotmail.com>
2024-03-04 00:45:17 -0700

    BD
    Logica de la BD, Modelos.

---
+++ database/migrations/2024_03_04_055923_create_tournaments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tournaments', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('id_league');
+            $table->foreign('id_league')->references('id')->on('leagues');
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tournaments');
+    }
+};
---
+++ database/migrations/2024_03_04_065751_create_games_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('league_id');
+            $table->unsignedBigInteger('tournament_id');
+            $table->unsignedBigInteger('home_team_id');
+            $table->unsignedBigInteger('away_team_id');
+            $table->unsignedBigInteger('field_id');
+            $table->unsignedBigInteger('referee_id');
+            $table->unsignedBigInteger('schedule_id');
+            $table->string('time');
+            $table->foreign('league_id')->references('id')->on('leagues');
+            $table->foreign('tournament_id')->references('id')->on('tournaments');
+            $table->foreign('home_team_id')->references('id')->on('teams');
+            $table->foreign('away_team_id')->references('id')->on('teams');
+            $table->foreign('field_id')->references('id')->on('fields');
+            $table->foreign('referee_id')->references('id')->on('referees');
+            $table->foreign('schedule_id')->references('id')->on('schedules');
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('games');
+    }
+};
